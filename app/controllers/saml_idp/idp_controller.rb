11661dd13b49f043b59ce70ee0e20293b312f91c
Pete <jimworm@gmail.com>
2012-10-17 17:03:06 +0200

    defined controller class with different syntax

--- app/controllers/saml_idp/idp_controller.rb
+++ app/controllers/saml_idp/idp_controller.rb
@@ -1,48 +1,46 @@
 # encoding: utf-8
-module SamlIdp
-  class IdpController < ActionController::Base
-    include SamlIdp::ControllerMethods
+class SamlIdp::IdpController < ActionController::Base
+  include SamlIdp::ControllerMethods
 
-    unloadable
-    protect_from_forgery
+  unloadable
+  protect_from_forgery
 
-    before_filter :validate_saml_request
+  before_filter :validate_saml_request
 
-    def new
-      render :template => "saml_idp/idp/new"
+  def new
+    render :template => "saml_idp/idp/new"
+  end
+
+  def create
+    if @saml_response = encode_SAMLResponse(id_resource, decoded_saml_request)
+      render('saml_idp/idp/saml_post', layout: false)
+    else
+      render 'saml_idp/idp/new'
     end
+  end
 
-    def create
-      if @saml_response = encode_SAMLResponse(id_resource, decoded_saml_request)
-        render('saml_idp/idp/saml_post', layout: false)
-      else
-        render 'saml_idp/idp/new'
-      end
-    end
-
-    protected
-    def id_resource
-      fail NotImplementedError
-    end
-    
-    def saml_request
-      fail NotImplementedError
-    end
-    helper_method :saml_request
+  protected
+  def id_resource
+    fail NotImplementedError
+  end
   
-    def decoded_saml_request
-      @decoded_saml_request ||= decode_SAMLRequest(saml_request)
-    rescue => e
-      logger.warn "SAML request failed to decode: #{e.message}"
-      logger.warn "Original SAML request: '#{saml_request}`"
-      raise e
-    end
-    helper_method :decoded_saml_request
-  
-    def validate_saml_request
-      decoded_saml_request
-    rescue
-      render 'saml_idp/idp/error' and return
-    end
+  def saml_request
+    fail NotImplementedError
+  end
+  helper_method :saml_request
+
+  def decoded_saml_request
+    @decoded_saml_request ||= decode_SAMLRequest(saml_request)
+  rescue => e
+    logger.warn "SAML request failed to decode: #{e.message}"
+    logger.warn "Original SAML request: '#{saml_request}`"
+    raise e
+  end
+  helper_method :decoded_saml_request
+
+  def validate_saml_request
+    decoded_saml_request
+  rescue
+    render 'saml_idp/idp/error' and return
   end
 end
