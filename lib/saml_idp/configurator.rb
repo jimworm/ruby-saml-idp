0bd1203c47d4e222773ad27e36aef9a597ede8a2
Pete <jimworm@gmail.com>
2012-10-18 11:35:22 +0200

    now using a hash to configure rather than a file eval

--- lib/saml_idp/configurator.rb
+++ lib/saml_idp/configurator.rb
@@ -3,11 +3,16 @@ module SamlIdp
   class Configurator
     attr_accessor :x509_certificate, :secret_key, :algorithm
 
-    def initialize(config_file = nil)
-      self.x509_certificate = Default::X509_CERTIFICATE
-      self.secret_key = Default::SECRET_KEY
-      self.algorithm = :sha1
-      instance_eval(File.read(config_file), config_file) if config_file
+    def initialize(options = {})
+      defaults = {
+        x509_certificate: Default::X509_CERTIFICATE,
+        secret_key: Default::SECRET_KEY,
+        algorithm: :sha1
+      }
+      options = defaults.merge(options)
+      self.x509_certificate = options[:x509_certificate]
+      self.secret_key = options[:secret_key]
+      self.algorithm = options[:algorithm]
     end
   end
 end
